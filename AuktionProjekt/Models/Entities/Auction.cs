472cb93e92e8a70b9ef363d97b357e3cd858d1d1
CorneliaB89 <cornelia.buiuc@gmail.com>
2024-01-15 09:38:10 +0100

    Entities properties

--- AuktionProjekt/Models/Entities/Auction.cs
+++ AuktionProjekt/Models/Entities/Auction.cs
@@ -2,5 +2,13 @@
 {
     public class Auction
     {
+        public int AuctionID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public User User { get; set; }
+
     }
 }

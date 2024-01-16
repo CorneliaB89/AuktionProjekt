d8de148eaa517d557a293ef9f8525960db74e1b9
Mimmi G <Mimmigebrtsadik@gmail.com>
2024-01-16 20:20:55 +0100

    DBContext and update of Controllers.

--- AuktionProjekt/Controllers/AuctionController.cs
+++ AuktionProjekt/Controllers/AuctionController.cs
@@ -20,8 +20,7 @@ namespace AuktionProjekt.Controllers
 
         [HttpGet("{auctionId}")]
         [Authorize]
-        public IActionResult GetAuctionDetails(int auctionId) //Kanske inte behöver .............
-        {
+        public IActionResult GetAuctionDetails(int auctionId) //Kanske inte behöver, (Behövs för att få fram detaljer i auctionen.
             // Logic to get details of a specific auction
             return Ok();
         }
@@ -33,29 +32,7 @@ namespace AuktionProjekt.Controllers
             // Logic to get a list of all auctions
             return Ok();
         }
-
-        [HttpGet("{auctionId}/bids")]
-        [Authorize]
-        public IActionResult GetBidsForAuction(int auctionId) //Har flyttat denna i bidControllern (kan ta bort) ..........
-        {
-            // Logic to get all bids for a specific auction
-            return Ok();
-        }
-
-        [HttpGet("{auctionId}/user")]
-        [Authorize]
-        public IActionResult GetAuctionOwner(int auctionId)  //Behövs inte (kan ta bort) ...........
-        {
-            // Logic to get the user (owner) of a specific auction
-            return Ok();
-        }
-
-        [HttpDelete("{auctionId}")]
-        [Authorize]
-        public IActionResult CloseAuction(int auctionId)  // kommer ej behövas (kan ta bort) ..........
-        {
-            // Logic to close an auction
-            return Ok();
+       
         }
         [HttpGet]
         public IActionResult SearchAuction(string title) 

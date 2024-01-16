2d8856cf0fbc383d706d26ff5cebdc1faab98481
CorneliaB89 <cornelia.buiuc@gmail.com>
2024-01-16 20:36:45 +0100

    Har testat göra bidrepo för get bid

--- AuktionProjekt/Repository/Repo/BidRepo.cs
+++ AuktionProjekt/Repository/Repo/BidRepo.cs
@@ -1,6 +1,24 @@
-﻿namespace AuktionProjekt.Repository.Repo
+﻿using AuktionProjekt.Models.Entities;
+using System.Data;
+
+namespace AuktionProjekt.Repository.Repo
 {
     public class BidRepo
     {
+        public List<Auction> GetBid() //Osäker om jag har tänkt rätt
+        {
+            using (IDbConnection db = /*Databasconnectionstring*/)
+            {
+                
+                var searchedBids = db.Query<Auction, Bid, Auction>("GetBid",
+                      (auctions, bids) =>
+                      {
+                          auctions.Bids = bids;
+                          return bids;
+
+                      }, splitOn: "AuctionID", commandType: CommandType.StoredProcedure).ToList();
+                return searchedBids;
+            }
+        }
     }
 }
